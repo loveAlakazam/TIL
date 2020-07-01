862e01843b16567c7f20eb52aa0e9e9abef5add4
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2020-07-01 17:39:00 +0900

    Create sample_server_for_map.py

---
+++ 2020/0701/sample_server_for_map.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, jsonify
+
+app=Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def main_page():
+    return render_template('kakao_map_sample.html')
+
+@app.route('/show/map', methods=['GET'])
+def show_map():
+    return jsonify({'result': 'success'})
+
+
+if __name__=='__main__':
+    app.run('0.0.0.0', 포트번호 ,debug=True)
