324fac5c1f3d5fe5e6b6520f7f4bf257d68ccb59
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2020-07-15 15:14:02 +0900

    Create Runner.java

---
+++ 2020/KH_Git_Repository/review_for_java/Collections/Runner.java
@@ -0,0 +1,17 @@
+package runner;
+import list.LinkedList1;
+
+
+public class Runner {
+
+	public static void main(String[] args) {
+	
+		// LinkedList1객체를 생성 => 메소드 doList1() 실행
+//		new LinkedList1().doList1();
+		
+		new LinkedList1().doList2();
+		
+//		new LinkedList1().doList3();
+	}
+
+}
