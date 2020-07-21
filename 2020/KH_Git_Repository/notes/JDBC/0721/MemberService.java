8cd217d4267028a9166e802c4dc023fb4ef19e97
loveAlakazam <36457434+loveAlakazam@users.noreply.github.com>
2020-07-21 18:43:01 +0900

    0721-update/delete self homework

--- 2020/KH_Git_Repository/notes/JDBC/0721/MemberService.java
+++ 2020/KH_Git_Repository/notes/JDBC/0721/MemberService.java
@@ -66,9 +66,15 @@ public class MemberService {
 	}
 
 	public int checkMemberId(String memberId) {
+		// Connection객체를 만든다.
 		Connection conn = getConnection();
+		
+		// 데이터베이스와 연결할 준비를 한다.
 		MemberDAO mDAO= new MemberDAO();
 		
+		// Connection객체와 memberId를 받아서 데이터베이스에서 select 쿼리문을 수행한다.
+		// 쿼리문 수행 결과를 int타입으로 받는다 =>(성공) 1 (아이디1개) / (실패) 0
+		// 입력받은 아이디가 존재하는지 확인
 		int check=mDAO.checkMember(conn, memberId);
 		return check;
 	}
@@ -77,8 +83,25 @@ public class MemberService {
 		Connection conn= getConnection();
 		MemberDAO mDAO = new MemberDAO();
 		
+		// 멤버 정보 변경에 대한 쿼리문이 성공했는지 확인한다.
 		int result= mDAO.updateMember(conn, sel, memberId, input);
 		if(result>0) {
+			//변경이 성공했다면 commit을 (상태저장)
+			commit(conn);
+		}else {
+			//변경이 실패했다면, 데이터변경이전으로 롤백.
+			rollback(conn);
+		}
+		return result;
+	}
+
+	public int deleteMember(String memberId) {
+		Connection conn= getConnection(); //Connection을 만든다.
+		MemberDAO mDAO = new MemberDAO(); //MemberDAO객체를 만든다.
+		
+		int result=mDAO.deleteMember(conn, memberId);
+		if(result>0) {
+			//삭제결과가 성공하면
 			commit(conn);
 		}else {
 			rollback(conn);
