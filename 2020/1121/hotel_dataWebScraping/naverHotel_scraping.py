ce7a94114707c302f7dcaf6067a45e33efb2ccd3
loveAlakazam <dmsrkd1216@gmail.com>
2020-11-22 16:29:56 +0900

    2020.11.22 - 호텔데이터 selenium성공

--- 2020/1121/hotel_dataWebScraping/naverHotel_scraping.py
+++ 2020/1121/hotel_dataWebScraping/naverHotel_scraping.py
@@ -43,44 +43,48 @@ localCode = {
 }
 
 
+global options
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument('disable-gpu')
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36")
+    
+
 def get_hotel_info(key):
     one_hotel_info = {}
 
-    options = webdriver.ChromeOptions()
-    #options.add_argument('headless')
-    options.add_argument('disable-gpu')
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36")
-    driver = webdriver.Chrome('./chromedriver_win32/chromedriver.exe', options=options)
+    driver = webdriver.Chrome('chromedriver.exe', options=options)
     
     # 호텔디테일 페이지 url
     url = 'https://hotel.naver.com/hotels/item?hotelId='+key
     driver.get(url)
+    time.sleep(1) #페이지 로드까지 1초 대기
     
     req = driver.page_source
     soup = BeautifulSoup(req, 'html.parser')
-    driver.implicitly_wait(3) #페이지 로드까지 3초 대기
-    
-    driver.find_element_by_class_name('more').click()  # 더보기 버튼을 누른다.
-    driver.implicitly_wait(1)  # 예약페이지 로드 1초 대기
 
     # 호텔이름
     hotel_name = soup.select_one('div.info_wrap > strong.hotel_name').text
     one_hotel_info['BO_TITLE'] = hotel_name
 
     # 호텔등급
-    hotel_rank = soup.select_one('div.info_wrap > span.grade').text
+    hotel_rank = soup.find('span', class_='grade').text
     if hotel_rank in ['1성급', '2성급', '3성급', '4성급', '5성급']:
         one_hotel_info['HOTEL_RANK']=int(hotel_rank[0])
     else:
         one_hotel_info['HOTEL_RANK']=None
         
-   
     # 호텔주소
-    hotel_addr=soup.select_one('body > div > div > div.container.ng-scope > div.content > div.hotel_info_area > div.info_wrap.type_fix > div:nth-child(5) > p.addr')
-    one_hotel_info['HOTEL_ADDRESS']=hotel_addr.text
+    hotel_addr_list= [addr for addr in soup.select_one('p.addr').text.split(', ')]
+    one_hotel_info['HOTEL_ADDRESS']=hotel_addr_list[0]
+    one_hotel_info['HOTEL_LOCAL_CODE']=localCode[hotel_addr_list[-2]]
     
     # 호텔소개
-    hotel_content = soup.select_one('div.desc_wrap > p.txt')
+    driver.find_element_by_class_name('more').click()  # 더보기 버튼을 누른다.
+    time.sleep(1)  # 예약페이지 로드 1초 대기
+    
+    hotel_content = soup.select_one('div.desc_wrap.ng-scope > p.txt.ng-binding')
     one_hotel_info['BO_CONTENT'] = hotel_content.text
 
     # 호텔옵션
@@ -88,7 +92,6 @@ def get_hotel_info(key):
     one_hotel_info['HOTEL_REAL_OPTIONS'] = hotel_options
     
     driver.quit()
-    
     return one_hotel_info
 
 
