9fc373b4afd568bba53ceb54d20c20b32006ef9e
loveAlakazam <dmsrkd1216@gmail.com>
2022-12-23 01:13:23 +0900

    feat: PUT/PATCH/DELETE API 내부로직 작성완료

--- 2022/NestJS_Basics/section1/02/src/cats/cats.route.ts
+++ 2022/NestJS_Basics/section1/02/src/cats/cats.route.ts
@@ -55,4 +55,75 @@ router.post("/cats", (req, res) => {
   }
 });
 
+// UPDATE[PUT]: 전체 업데이트
+router.put("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+
+    const cat = Cat.forEach((cat) => {
+      if (cat.id === params.id) {
+        cat = body; // 수정
+        return res.status(201).send({
+          success: true,
+          data: cat,
+        });
+      }
+    });
+
+    throw new Error("고양이 데이터가 존재하지 않아서 실패하였습니다.");
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+// UPDATE[PATCH]: 고양이 데이터를 부분적으로 업데이트
+router.patch("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+
+    let result;
+    const cat = Cat.forEach((cat) => {
+      if (cat.id === params.id) {
+        cat = { ...cat, ...body }; // body에 해당된 부분만 수정
+        result = cat;
+        return res.status(201).send({
+          success: true,
+          data: result,
+        });
+      }
+    });
+
+    throw new Error("고양이 데이터가 존재하지 않아서 실패하였습니다.");
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+// DELETE: 고양이 데이터 삭제
+router.delete("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    console.log(params);
+    const cats = Cat.filter((cat) => cat.id !== params.id);
+
+    return res.status(201).send({
+      success: true,
+      data: cats,
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
 export default router;
